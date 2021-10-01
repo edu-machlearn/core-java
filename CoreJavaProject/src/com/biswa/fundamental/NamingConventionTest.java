188e6aa86d4ba07828505a011ad55d26b4fc14dc
Biswaranjan Mohapatra <info.machlearn@gmail.com>
2021-10-02 02:06:00 +0530

    Test

--- CoreJavaProject/src/com/biswa/fundamental/NamingConventionTest.java
+++ CoreJavaProject/src/com/biswa/fundamental/NamingConventionTest.java
@@ -2,6 +2,6 @@ package com.biswa.fundamental;
 
 public class NamingConventionTest {
 	public static void main(String[] args) {
-		System.out.println("Naming Convention Test..");
+		System.out.println("Naming Convention Test");
 	}
 }

cae2656e3ada59ec5120e5cc8703a66481137f47
Biswaranjan Mohapatra <info.machlearn@gmail.com>
2021-10-02 21:44:07 +0530

    1

--- CoreJavaProject/src/com/biswa/fundamental/DataTypeVariableTest.java
+++ CoreJavaProject/src/com/biswa/fundamental/DataTypeVariableTest.java
@@ -1,17 +1,15 @@
 package com.biswa.fundamental;
 
 public class DataTypeVariableTest {
-
 	public static void main(String[] args) {
-        String name= "Biswaranjan Mohapatra";
-        int roll = 1111;
-        float salaryOfEmployee=20000.00F;
-        boolean b = true;
-        char a ='M';
-
-        System.out.println(name);
-        System.out.println(roll);
-        System.out.println(b);
+		String name = "Biswaranjan Mohapatra";
+		int roll = 1111;
+		float salaryOfEmployee = 20000.00F;
+		boolean b = true;
+		char a = 'M';
+		System.out.println(name);
+		System.out.println(roll);
+		System.out.println(b);
 		System.out.println(salaryOfEmployee);
 		System.out.println(a);
 	}

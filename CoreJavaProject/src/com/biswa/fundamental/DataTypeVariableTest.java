65f6d5fa35d2792c50a51b565839a8864bc216e4
Biswaranjan Mohapatra <info.machlearn@gmail.com>
2021-10-02 20:29:13 +0530

    DataTypeVariable Test

---
+++ CoreJavaProject/src/com/biswa/fundamental/DataTypeVariableTest.java
@@ -0,0 +1,17 @@
+package com.biswa.fundamental;
+
+public class DataTypeVariableTest {
+
+	public static void main(String[] args) {
+        String name= "Biswaranjan Mohapatra";
+        float roll = 1111.0F;
+
+        boolean b = true;
+
+        System.out.println(name);
+        System.out.println(roll);
+        System.out.println(b);
+		System.out.println();
+	}
+
+}

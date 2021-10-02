d80a9c6fc39548dba54abb9883e061ebadec1358
Biswaranjan Mohapatra <info.machlearn@gmail.com>
2021-10-02 21:28:19 +0530

    DataTypeVariable Test Bu

--- CoreJavaProject/src/com/biswa/fundamental/DataTypeVariableTest.java
+++ CoreJavaProject/src/com/biswa/fundamental/DataTypeVariableTest.java
@@ -7,11 +7,13 @@ public class DataTypeVariableTest {
         int roll = 1111;
         float salaryOfEmployee=20000.00F;
         boolean b = true;
+        char a ='M';
 
         System.out.println(name);
         System.out.println(roll);
         System.out.println(b);
 		System.out.println(salaryOfEmployee);
+		System.out.println(a);
 	}
 
 }

40bee042c8a2e22a6316b3dcc38c31d71b962b24
Biswaranjan Mohapatra <info.machlearn@gmail.com>
2021-10-02 08:37:44 +0530

    ArithmaticTest

---
+++ CoreJavaProject/src/com/biswa/operator/ArithmaticTest.java
@@ -0,0 +1,10 @@
+package com.biswa.operator;
+
+public class ArithmaticTest {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 2;
+		System.out.println("Addition = " + (a + b));
+	}
+
+}

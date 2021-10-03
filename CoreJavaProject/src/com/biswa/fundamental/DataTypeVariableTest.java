90476d924e808673310951a628f5f8a8144d3a36
Biswaranjan Mohapatra <info.machlearn@gmail.com>
2021-10-03 19:57:05 +0530

    Escape Sequence

--- CoreJavaProject/src/com/biswa/fundamental/DataTypeVariableTest.java
+++ CoreJavaProject/src/com/biswa/fundamental/DataTypeVariableTest.java
@@ -1,17 +1,28 @@
+/*
+This is a Program that demonstrate DataTypeVariableTest
+Auther : Biswaranjan
+Date : 3 OCT 2021
+*/
+
+/* multiple comment line     */
+// Single Comment line
 package com.biswa.fundamental;
 
 public class DataTypeVariableTest {
 	public static void main(String[] args) {
 		String name = "Biswaranjan Mohapatra";
-		int roll = 1111;
+		int roll = 1111;//This is roll number of Student
 		float salaryOfEmployee = 20000.00F;
 		boolean b = true;
 		char a = 'M';
 		System.out.println(name);
+		
+		/*
 		System.out.println(roll);
 		System.out.println(b);
 		System.out.println(salaryOfEmployee);
 		System.out.println(a);
 		System.out.println("Name :"+name);
+		*/
 	}
 }
---
+++ CoreJavaProject/src/com/biswa/fundamental/EscapeSequenceTest.java
@@ -0,0 +1,27 @@
+/*
+This Program Demonstrate EscapeSequence
+EscapeSequence - Special Character used in Java
+1. \n - Create a New line
+2. \t - Create tab
+3. \"Hello!\" How are you?
+4. Ram\'s Dog
+*/
+package com.biswa.fundamental;
+
+public class EscapeSequenceTest {
+
+	public static void main(String[] args) {
+		System.out.println("Biswaranjan Mohapatra");
+		System.out.println("Age : 35");
+		System.out.println("Address : New Delhi");
+		System.out.println("----------");
+		System.out.println("Biswaranjan Mohapatra \nAge : 35 \nAddress : New Delhi");
+		System.out.println("Name \tAddress \tAge");
+		
+		System.out.println("\"Hello!\" How are you?");
+		System.out.println("Ram\'s Dog");
+		
+		
+	}
+
+}
